0e4ff779679ea99c1072b24868d19db48a599ab0
maduranga95 <maduranga95@gmail.com>
2022-04-04 11:41:14 +0530

    rukmal commit

--- main.py
+++ main.py
@@ -3,31 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import json_hash
+import middleware
 import operations
-import json
-from blockchain.blockchain import Blockchain
-from blockchain.transaction import Transaction,TransactionEncoder
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-blockchain =  Blockchain()
-
-def add_transaction(request_data):
-    #request_data = request.get_json()
-    transaction = Transaction(request_data['id'],request_data['userID'],request_data['timestamp'],request_data['data'])
-    blockchain.add_new_transaction(TransactionEncoder().encode(transaction))
-    return "True"
-
-def get_chain():
-    chain_data = []
-    for block in blockchain.chain:
-        chain_data.append(block.__dict__)
-    return json.dumps({"length": len(chain_data),
-                       "chain": chain_data})
-def mine():
-    blockchain.mine()
-    return "True"
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
@@ -36,14 +13,11 @@ if __name__ == '__main__':
          "timestamp": operations.get_time(),
          "data":{
                     "name":"Rukmal1",
-                    "balance":"$300"
+                    "balance":"$500"
                 }
          }
-    print(add_transaction(y))
-    #print(add_transaction(y))
 
-    print(mine())
-    print(get_chain())
+    print(middleware.update(y))
     #search_item = transaction.update(y)
     #operations.update(y)
     #operations.insert(y)
---
+++ middleware.py
@@ -0,0 +1,41 @@
+import operations
+import json
+from blockchain.blockchain import Blockchain
+from blockchain.transaction import Transaction,TransactionEncoder
+
+blockchain =  Blockchain()
+
+def add_transaction(request_data):
+    #request_data = request.get_json()
+    transaction = Transaction(request_data['id'],request_data['userID'],request_data['timestamp'],request_data['data'])
+    blockchain.add_new_transaction(TransactionEncoder().encode(transaction))
+    return "True"
+
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+def mine():
+    blockchain.mine()
+    return "True"
+
+def update(req_data):
+    trans = add_transaction(req_data)
+    if trans == "True":
+        operations.update(req_data)
+        print(mine())
+        print(get_chain())
+        return "Successful!"
+
+    else:
+        return "Unsucessful!"
+
+def insert(req_data):
+    trans = add_transaction(req_data)
+    if trans == "True":
+        operations.insert(req_data)
+        return "Successful!"
+    else:
+        return "Unsucessful!"
